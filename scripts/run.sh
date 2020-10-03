13371798d9bdec77399af569f4f114ac82195c19
seungh0 <will.seungho@gmail.com>
2020-10-03 23:43:25 +0900

    feat: blue-green 무중단 배포

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+DOCKER_APP_NAME=potato-backend
+
 cd /home/ubuntu/build
 
-docker-compose down
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 788906588229.dkr.ecr.ap-northeast-2.amazonaws.com
 
-sleep 10
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
-docker-compose up -d --build
+if [ -z "$EXIST_BLUE" ]; then
+    echo "Blue Up"
+    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+
+    sleep 30
+
+    echo "Green Down"
+    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+else
+    echo "Green Up"
+    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+
+    sleep 30
+
+    echo "Blue Down"
+    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+fi
