681a9abd71d74e89e72120f3a0667b0822a124bb
seungh0 <will.seungho@gmail.com>
2020-11-06 16:12:31 +0900

    memberRepository test

---
+++ test/repository/memberRepository.test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import * as memberRepository from '../../src/repository/memberRepository';
+import { sequelize, models } from '../../src/model';
+
+describe('Member Repository', function () {
+  beforeEach(() => sequelize.sync({ force: true }));
+
+  describe('findByEmail()', function () {
+    it('해당 이메일을 가진 멤버가 반환된다', async function () {
+      // given
+      const email = 'will.seungho@gmail.com';
+      const name = 'alice';
+      const profileUrl = 'http://profileUrl.com';
+
+      const member = { email: email, name: name, profileUrl: profileUrl };
+      models.Member.bulkCreate([member]);
+
+      // when
+      const findMember = await memberRepository.findMemberByEmail(email);
+
+      // then
+      expect(findMember.dataValues.email).to.eq(email);
+      expect(findMember.dataValues.name).to.eq(name);
+      expect(findMember.dataValues.profileUrl).to.eq(profileUrl);
+    });
+  });
+});
