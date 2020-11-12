6cef92ee7c962c586a08337bda30f386f261c8b6
will0212 <test.seungho@gmail.com>
2020-11-13 00:53:06 +0900

    어떤 파일도 요청되지 않았을 경우 예외처리

--- src/routes/upload/uploadController.js
+++ src/routes/upload/uploadController.js
@@ -1,8 +1,12 @@
 import { ApiResponse } from '@src/ApiResponse';
 import { logger } from '@src/config/winston';
+import { NotFoundException } from '@src/exception/CustomException';
 
 export const uploadS3 = (req, res, next) => {
   try {
+    if (!req.file) {
+      throw new NotFoundException('업로드 할 파일이 존재하지 않습니다');
+    }
     logger.info(`파일이 업로드 되었습니다. ${req.file}`);
     res.status(200).send(new ApiResponse(req.file.location));
   } catch (err) {
