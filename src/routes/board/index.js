bca133e0fcc00495a574483dd1e3dfc731087723
yerimkoko <gochi97@naver.com>
2020-11-16 20:34:00 +0900

    delete 기능 추가

--- src/routes/board/index.js
+++ src/routes/board/index.js
@@ -21,14 +21,17 @@ router.post('/api/v1/product', (req, res) => {
   const senderName = req.body.senderName;
   const content = req.body.content;
 
-  db.raw(`INSERT INTO product(name, price, profile_url, sender_name, content) VALUES("${name}", ${price}, "${profileUrl}", "${senderName}", "${content}")`)
+  db.raw(
+    `INSERT INTO product(name, price, profile_url, sender_name, content) VALUES("${name}", ${price}, "${profileUrl}", "${senderName}", "${content}")`
+  )
     .then((response) => {
-      console.log(response)
-      res.status(200).send("Ok");
-    }).catch((error) => {
-      console.log(error);
-      res.status(500).end("에러발생!!!!")
+      console.log(response);
+      res.status(200).send('Ok');
     })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).end('에러발생!!!!');
+    });
 });
 
 /**
@@ -36,13 +39,11 @@ router.post('/api/v1/product', (req, res) => {
  * HTTP GET /api/v1/products
  */
 router.get('/api/v1/products', (req, res) => {
-  db.raw(`SELECT * FROM product`)
-    .then((response) => {
-      console.log(response[0])
-      res.status(200).send(response[0])
-    });
-})
-
+  db.raw(`SELECT * FROM product`).then((response) => {
+    console.log(response[0]);
+    res.status(200).send(response[0]);
+  });
+});
 
 /**
  * 특정 중고 거래 물건을 조회하는 API
@@ -52,12 +53,47 @@ router.get('/api/v1/product', (req, res) => {
   const productId = req.query.productId;
   db.raw(`SELECT id, name, price, content FROM product WHERE id = ${productId}`)
     .then((response) => {
-      console.log(response[0])
-      res.status(200).send(response[0])
-    }).catch((error) => {
-      console.log(error)
-      res.status(500).end("에러발생....")
+      console.log(response[0]);
+      res.status(200).send(response[0]);
     })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).end('에러발생....');
+    });
 });
 
+router.delete('/api/v1/product', (req, res) => {
+  const productId = req.query.productId;
+  db.raw(`SELECT id FROM product WHERE id =${productId}`).then((response) => {
+    if (response[0].length === 0) {
+      res.status(404).send('해당 상품을 찾을 수 없어요');
+    }
+    db.raw(`DELETE FROM product WHERE id = ${productId}`)
+      .then(() => {
+        res.status(200).send('OK');
+      })
+      .catch((error) => {
+        res.status(500).end('에러발생..');
+      });
+  });
+
+  /*router.put('/api/vi/product', (req, res) => {
+    const productId = req.query.productId;
+    db.raw(`SELECT id FROM WHERE id = ${product}`).then((response) => {
+      if (response[0] === 0) {
+        res.status(404).send('수정할 수 없습니다.');
+      }
+      db.raw(
+        `UPDATE product SET name, price, profileUrl, senderName, content = "${name}", ${price}, "${profileUrl}", "${senderName}", "${content}"`
+      )
+        .then((response) => {
+          res.status(200).log(response);
+        })
+        .catch((error) => {
+          res.status(500).end('에러발생');
+        });
+    });
+  });
+  */
+});
 export default router;
